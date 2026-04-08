edaddff3426d8a4d9c32f3f8e91f0170c8d853f9
alvaro may filho <alvaromayfilho30@gmail.com>
2026-04-07 20:32:32 -0400

    Initial Commit

---
+++ Solidarizar/SolidarizarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidarizarApp.swift
+//  Solidarizar
+//
+//  Created by Alvaro May Filho on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SolidarizarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
